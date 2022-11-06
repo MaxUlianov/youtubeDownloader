7cce2a9f16b6d623debdf807007056775eeea1da
MaxUlianov <curze.curze22@gmail.com>
2022-11-06 17:55:42 +0100

    Fixed page header, added POST form for link and download options, changed logo image files

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = Flask(__name__, static_folder="static", static_url_path="")
 
 @app.route('/', methods=['GET', 'POST'])
 def ytdl():
-    if request.method == "GET":
+    if request.method == "POST":
+        link = request.form["link-field"]
+        print(link)
+        print(request.form)
+        return render_template("page.html", options=link)
+    elif request.method == "GET":
         return render_template("page.html")
     return
 
