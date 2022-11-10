3c05f41fe4804e6cfc08551edd6b92e6cd7e4b70
MaxUlianov <curze.curze22@gmail.com>
2022-11-10 21:22:26 +0100

    Edited html main page, fixed some css; added instruction placeholders; added vieo download options check with loading options to web page.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+from pytube.exceptions import RegexMatchError
+
+from downloader import get_video_options
 
 
 app = Flask(__name__, static_folder="static", static_url_path="")
@@ -10,10 +13,23 @@ def ytdl():
         link = request.form["link-field"]
         print(link)
         print(request.form)
-        return render_template("page.html", options=link)
+
+        options = []
+        try:
+            options = get_video_options(link)
+        except RegexMatchError as e:
+            error(repr(e))
+            options = [repr(e)]
+        return render_template("page.html", options=options)
+
     elif request.method == "GET":
         return render_template("page.html")
     return
+
+
+@app.route('/error', methods=["GET"])
+def error(e):
+    return f'Error {e} in video download'
 
 
 if __name__ == '__main__':
